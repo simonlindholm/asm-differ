bb865fa4631f5d8e674fc09d17da5334339a4e6d
Snuffy <mattsasa@me.com>
2022-05-28 23:33:49 -0500

    update imm_matches_everything for ppc to not catch real symbol names with @ symbols

--- diff.py
+++ diff.py
@@ -2076,7 +2076,12 @@ def normalize_stack(row: str, arch: ArchSettings) -> str:
 
 def imm_matches_everything(row: str, arch: ArchSettings) -> bool:
     if arch.name == "ppc":
-        return any(str in row for str in ["...data", "@"])
+        if "...data" in row:
+            return True
+
+        row = "".join(row.rsplit("@ha", 1))
+        row = "".join(row.rsplit("@l", 1))
+        return re.search(re.compile(r"\A@\d+\Z"), row) != None
     else:
         return "(." in row
 
