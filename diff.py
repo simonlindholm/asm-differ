24c4776363c2a992def79419b82d66565da16c04
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-09-04 17:09:24 +0100

    jump_instructions

--- diff.py
+++ diff.py
@@ -1739,6 +1739,7 @@ class ArchSettings:
     re_reloc: Pattern[str]
     branch_instructions: Set[str]
     instructions_with_address_immediates: Set[str]
+    jump_instructions: Set[str] = field(default_factory=set)
     forbidden: Set[str] = field(default_factory=lambda: set(string.ascii_letters + "_"))
     arch_flags: List[str] = field(default_factory=list)
     branch_likely_instructions: Set[str] = field(default_factory=set)
@@ -1773,6 +1774,18 @@ MIPS_BRANCH_INSTRUCTIONS = MIPS_BRANCH_LIKELY_INSTRUCTIONS.union(
         "bc1t",
         "bc1f",
     }
+)
+
+MIPS_JUMP_ADDR_INSTRUCTIONS = {
+    "jal",
+    "j",
+}
+MIPS_JUMP_REG_INSTRUCTIONS = {
+    "jalr",
+    "jr",
+}
+MIPS_JUMP_INSTRUCTIONS = set.union(
+    MIPS_JUMP_ADDR_INSTRUCTIONS, MIPS_JUMP_REG_INSTRUCTIONS
 )
 
 ARM32_PREFIXES = {"b", "bl"}
@@ -1938,8 +1951,11 @@ MIPS_SETTINGS = ArchSettings(
     arch_flags=["-m", "mips:4300"],
     branch_likely_instructions=MIPS_BRANCH_LIKELY_INSTRUCTIONS,
     branch_instructions=MIPS_BRANCH_INSTRUCTIONS,
-    instructions_with_address_immediates=MIPS_BRANCH_INSTRUCTIONS.union({"jal", "j"}),
-    delay_slot_instructions=MIPS_BRANCH_INSTRUCTIONS.union({"j", "jal", "jr", "jalr"}),
+    jump_instructions=MIPS_JUMP_INSTRUCTIONS,
+    instructions_with_address_immediates=set.union(
+        MIPS_BRANCH_INSTRUCTIONS, MIPS_JUMP_ADDR_INSTRUCTIONS
+    ),
+    delay_slot_instructions=set.union(MIPS_BRANCH_INSTRUCTIONS, MIPS_JUMP_INSTRUCTIONS),
     proc=AsmProcessorMIPS,
 )
 
