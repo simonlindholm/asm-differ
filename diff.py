0282896c9a909997743ba71b3b16151c52e495da
EstexNT <estex64@gmail.com>
2023-06-26 09:28:08 +0300

    Mention SH4 in README and argparse

--- diff.py
+++ diff.py
@@ -54,7 +54,7 @@ if __name__ == "__main__":
         argcomplete = None
 
     parser = argparse.ArgumentParser(
-        description="Diff MIPS, PPC, AArch64, ARM32, or SH2 assembly."
+        description="Diff MIPS, PPC, AArch64, ARM32, SH2, or SH4 assembly."
     )
 
     start_argument = parser.add_argument(
