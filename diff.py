4e04d9dbedc27a5d0ac9d11146705695c5648612
1superchip <1superchip@gmail.com>
2026-08-19 15:19:27 -0500

    Add ip to arm32 re_reg

--- diff.py
+++ diff.py
@@ -2971,7 +2971,7 @@ ARM32_SETTINGS = ArchSettings(
     #   - VFP/NEON registers: s0..31, d0..31, q0..15, fpscr, fpexc, fpsid
     # SP should not be in this list.
     re_reg=re.compile(
-        r"\$?\b([rq][0-9]|[rq]1[0-5]|pc|lr|[ds][12]?[0-9]|[ds]3[01]|fp(scr|exc|sid))\b"
+        r"\$?\b([rq][0-9]|[rq]1[0-5]|pc|lr|ip|[ds][12]?[0-9]|[ds]3[01]|fp(scr|exc|sid))\b"
     ),
     re_sprel=re.compile(r"sp, #-?(0x[0-9a-fA-F]+|[0-9]+)\b"),
     re_large_imm=re.compile(r"-?[1-9][0-9]{2,}|-?0x[0-9a-f]{3,}"),
