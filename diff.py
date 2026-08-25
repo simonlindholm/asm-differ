c80ea4e3d16ecc8f4c21923633f3f9b1fb534c53
Chippy <57328807+1superchip@users.noreply.github.com>
2026-08-25 09:09:39 -0500

    Add sl and fp to arm32 re_reg (#224)

--- diff.py
+++ diff.py
@@ -2967,11 +2967,12 @@ ARM32_SETTINGS = ArchSettings(
     re_comment=re.compile(r"(<.*>|//.*$)"),
     # Includes:
     #   - General purpose registers: r0..13
+    #   - General purpose registers: sl (r10), fp (r11), ip (r12)
     #   - Frame pointer registers: lr (r14), pc (r15)
     #   - VFP/NEON registers: s0..31, d0..31, q0..15, fpscr, fpexc, fpsid
     # SP should not be in this list.
     re_reg=re.compile(
-        r"\$?\b([rq][0-9]|[rq]1[0-5]|pc|lr|ip|[ds][12]?[0-9]|[ds]3[01]|fp(scr|exc|sid))\b"
+        r"\$?\b([rq][0-9]|[rq]1[0-5]|sl|fp|ip|pc|lr|[ds][12]?[0-9]|[ds]3[01]|fp(scr|exc|sid))\b"
     ),
     re_sprel=re.compile(r"sp, #-?(0x[0-9a-fA-F]+|[0-9]+)\b"),
     re_large_imm=re.compile(r"-?[1-9][0-9]{2,}|-?0x[0-9a-f]{3,}"),
